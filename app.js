b78e68cc8fc4a81c7ffd27695be7a34c729580f1
kngsujng <kngsujng@naver.com>
2022-11-02 21:48:31 +0900

    로그인 페이지 구현
    로컬 스토리지를 이용한 데이터 저장 및 토글 기능 구현

---
+++ app.js
@@ -0,0 +1,35 @@
+//const loginForm = document.getElementById("login-form");
+//const loginInput = loginForm.querySelector("input");
+//const loginButton = loginForm.querySelector("button");
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+//const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+//JS는 함수의 인자에 대한 정보를 알려준다. 
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `HEllo ! ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername); 
+    //local Storage에 유저정보가 있으면 받아서 인자로 넣어준다. 
+}
