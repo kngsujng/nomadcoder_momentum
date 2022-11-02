2233a9b342038334eb34d89e39902a2355f9c6a3
kngsujng <kngsujng@naver.com>
2022-11-02 22:08:09 +0900

    리펙토링 : paintGreetings함수 인자 삭제

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username){
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `HEllo ! ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -30,6 +30,6 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername); 
+    paintGreetings(); 
     //local Storage에 유저정보가 있으면 받아서 인자로 넣어준다. 
 }
